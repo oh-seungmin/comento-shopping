dba44038778b84c8e5ad7c8928981ad055c87b68
oh-seungmin <gkahsl13@naver.com>
2022-07-10 17:56:40 +0900

    refactor : home pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,10 @@
-import React, { useState } from "react";
 import Navigaion from "../components/Navigation";
 import ThemeButton from "../components/ThemeButton";
 import ProductCard from "../components/ProductCard";
 import styled from "styled-components";
 
-
 const Home = ()=>{
-    const [products, setproducts] = useState();
-    
-    /*const onClickThemeButton = (themeId) =>{
-        if(themeId ==1){
-            setproducts(mockThemeProducts)
-        }else if(themeId==2){
-            setproducts(mockThemeProducts);
-        }
-    
-    }
-    */
-
+  
     return(
       <div>
         <div>
@@ -38,18 +25,21 @@ const Home = ()=>{
             description="쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙
             &화이트 비숑 머그잔입니다."
             thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg"
+            alt="비숑 블랙 머그잔" 
             />
             
             <ProductCard
             name="가열 보온 티코스터 온열 원터치 컵 받침대" 
             description="언제나 따뜻하게 최대 12시간 동안 최대 60도의 온도로 따뜻한 차를 즐길수 있습니다."
             thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg"
+            alt="가열 보온 티코스터 온열 원터치 컵 받침대" 
             />
         
             <ProductCard
             name="벨루즈까사 솜사탕 파스텔 머그 4종 세트" 
             description="솜삽탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그."
             thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg"
+            alt="벨루즈까사 솜사탕 파스텔 머그 4종 세트" 
             />
             </ProductSection>
         </div>
